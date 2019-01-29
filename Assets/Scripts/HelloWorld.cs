0f8d940efcc21c72cce7c2996232091f85d33115
sunfleck <dle5751@yahoo.ca>
2019-01-29 12:55:56 -0800

    Second commit
    Changes:
    1. Added a couple of skyboxes
    2. Added a 'hello world' script to demo how to add a script as a component
    3. Upgraded API compatability level (in Player Settings) to .NET 4.x from .NET standard 2.0. I had problems with VS on my desktop with the old level (although not on my laptop, where I installed VS as part of the Unity bundle).

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour
+{
+   // public GameObject textbox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Hello()
+    {
+        yield return new WaitForSeconds(5);
+        GetComponent<Text>().text = "HELLO WORLD!";
+    }
+}
