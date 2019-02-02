80d8952fc598dec435bd98bcce1d85ce13b4d5d0
sunfleck <dle5751@yahoo.ca>
2019-02-02 12:19:59 -0800

    Feb 2 commit
    1. Scenes for lesson 2 and lesson 3.
    2. Added a script subfolder containing three different methods for collision detection.

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour
+{
+    // can be used to demo how to add a script as a component (in this case, to a Text gameobject), or to demo the UI 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Hello");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Hello()
+    {
+        yield return new WaitForSeconds(5);
+        GetComponent<Text>().text = "HELLO WORLD!";
+    }
+}
