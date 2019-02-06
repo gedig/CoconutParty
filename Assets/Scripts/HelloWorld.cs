054701d9ff98c61f6b504699e4b5b78203237d16
Dylan Gedig <dylan@rednexus.games>
2019-02-06 14:34:53 -0800

    Bring merge changes over to this branch

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -5,12 +5,8 @@ using UnityEngine.UI;
 
 public class HelloWorld : MonoBehaviour
 {
-<<<<<<< HEAD
-   // public GameObject textbox;
-=======
     // can be used to demo how to add a script as a component (in this case, to a Text gameobject), or to demo the UI 
 
->>>>>>> 80d8952fc598dec435bd98bcce1d85ce13b4d5d0
     // Start is called before the first frame update
     void Start()
     {
