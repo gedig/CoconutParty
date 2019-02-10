bd327e48308cd1ec259f204bc5d2a405e4b48dc3
sunfleck <dle5751@yahoo.ca>
2019-02-10 14:16:47 -0800

    Feb 10 commit
    See my email.

---
+++ Assets/Scripts/Claire/Follow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 is needed to prevent the gameobject from tilting as it follows the target at close range  
+        Vector3 targetPosition = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);           
+        transform.LookAt(targetPosition);
+    }
+}
