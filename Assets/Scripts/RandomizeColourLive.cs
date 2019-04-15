39219ef6225a5249fc7118c0d923e7de7b2cd7ae
Dylan Gedig <dylan@rednexus.games>
2019-04-15 11:30:42 -0700

    Files created during Lesson 2 for live demos.

---
+++ Assets/Scripts/RandomizeColourLive.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeColourLive : MonoBehaviour
+{
+    public int ThisNumber;
+    private int _thisNumber;
+
+    private Renderer _renderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.gameObject.tag == "Projectile")
+        {
+            // Randomize The colour:
+            RandomizeColour();
+        }
+    }
+
+    /**
+     * This does Raycasting Logic for us. OnMouseDown is called when the user is looking at this
+     * object's collider and clicks their main mouse button.
+     */
+    void OnMouseDown()
+    {
+        RandomizeColour();
+    }
+
+    public void RandomizeColour()
+    {
+        float r = Random.Range(0f,1f);
+        float g = Random.Range(0f,1f);
+        float b = Random.Range(0.0f,1f);
+        Color newColour = new Color(r, g, b);
+        _renderer.material.color = newColour;
+    }
+}
